b846f0f88a258773dbcf353c02f2d1de18b24d0c
gn10 <8gn24gn25@gmail.com>
2024-10-19 11:37:04 +0900

    ピン名等見直し

--- F303PowerCutoff/Core/Inc/main.h
+++ F303PowerCutoff/Core/Inc/main.h
@@ -57,6 +57,32 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define DISCHARGE_Pin GPIO_PIN_1
+#define DISCHARGE_GPIO_Port GPIOA
+#define RELAY_EN_Pin GPIO_PIN_2
+#define RELAY_EN_GPIO_Port GPIOA
+#define LED_WIRED_STOP_Pin GPIO_PIN_4
+#define LED_WIRED_STOP_GPIO_Port GPIOA
+#define LED_REMOTE_STOP_Pin GPIO_PIN_5
+#define LED_REMOTE_STOP_GPIO_Port GPIOA
+#define LED_MODE_Pin GPIO_PIN_6
+#define LED_MODE_GPIO_Port GPIOA
+#define LED_ID1_Pin GPIO_PIN_7
+#define LED_ID1_GPIO_Port GPIOA
+#define ENABLE_Pin GPIO_PIN_8
+#define ENABLE_GPIO_Port GPIOA
+#define BUZZER_Pin GPIO_PIN_15
+#define BUZZER_GPIO_Port GPIOA
+#define DIP_1_Pin GPIO_PIN_3
+#define DIP_1_GPIO_Port GPIOB
+#define DIP_2_Pin GPIO_PIN_4
+#define DIP_2_GPIO_Port GPIOB
+#define SW_1_Pin GPIO_PIN_5
+#define SW_1_GPIO_Port GPIOB
+#define SW_2_Pin GPIO_PIN_6
+#define SW_2_GPIO_Port GPIOB
+#define SW_3_Pin GPIO_PIN_7
+#define SW_3_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
--- F303PowerCutoff/Core/Src/gpio.c
+++ F303PowerCutoff/Core/Src/gpio.c
@@ -42,8 +42,38 @@
 void MX_GPIO_Init(void)
 {
 
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, DISCHARGE_Pin|RELAY_EN_Pin|LED_WIRED_STOP_Pin|LED_REMOTE_STOP_Pin
+                          |LED_MODE_Pin|LED_ID1_Pin|BUZZER_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PAPin PAPin PAPin PAPin
+                           PAPin PAPin PAPin */
+  GPIO_InitStruct.Pin = DISCHARGE_Pin|RELAY_EN_Pin|LED_WIRED_STOP_Pin|LED_REMOTE_STOP_Pin
+                          |LED_MODE_Pin|LED_ID1_Pin|BUZZER_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PtPin */
+  GPIO_InitStruct.Pin = ENABLE_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(ENABLE_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PBPin PBPin PBPin PBPin
+                           PBPin */
+  GPIO_InitStruct.Pin = DIP_1_Pin|DIP_2_Pin|SW_1_Pin|SW_2_Pin
+                          |SW_3_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 }
 
